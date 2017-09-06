a4cdae2e9055d986a48d7c373a0bc69b8a0b906e
Eloi Pardo <eloipr94@gmail.com>
2017-09-06 15:08:19 +0200

    mirror and duplication modes features

---
+++ cura/DuplicatedNode.py
@@ -0,0 +1,71 @@
+from copy import deepcopy
+
+from UM.Math.Vector import Vector
+from UM.Operations.MirrorOperation import MirrorOperation
+from UM.Application import Application
+from UM.Scene.SceneNode import SceneNode
+
+from cura.Settings.ExtruderManager import ExtruderManager
+from cura.Settings.SetObjectExtruderOperation import SetObjectExtruderOperation
+
+class DuplicatedNode(SceneNode):
+
+    def __init__(self, node, parent = None):
+        super().__init__(parent)
+        self.node = node
+        self.setTransformation(node.getLocalTransformation())
+        self.setMeshData(node.getMeshData())
+        self.setVisible(deepcopy(node.isVisible()))
+        self._selectable = False
+        self._name = deepcopy(node.getName())
+        for decorator in node.getDecorators():
+            self.addDecorator(deepcopy(decorator))
+
+        for child in node.getChildren():
+            self.addChild(deepcopy(child))
+
+        node.calculateBoundingBoxMesh()
+        self.node.transformationChanged.connect(self._onTransformationChanged)
+        self.node.parentChanged.connect(self._someParentChanged)
+        self.parentChanged.connect(self._someParentChanged)
+        SetObjectExtruderOperation(self, ExtruderManager.getInstance().getExtruderStack(1).getId()).redo()
+
+    def setSelectable(self, select: bool):
+        self._selectable = False
+
+    def connect(self):
+        self.node.transformationChanged.connect(self._onTransformationChanged)
+
+    def disconnect(self):
+        self.node.transformationChanged.disconnect(self._onTransformationChanged)
+
+    def update(self):
+        if type(self.getParent()) == DuplicatedNode:
+            self.setTransformation(self.node.getLocalTransformation())
+            # self.setPosition(self.node.getPosition())
+            return
+        print_mode = Application.getInstance().getGlobalContainerStack().getProperty("print_mode", "value")
+        machine_width = Application.getInstance().getGlobalContainerStack().getProperty("machine_width", "value")
+        self.setScale(self.node.getScale())
+        node_pos = self.node.getPosition()
+        self.setTransformation(self.node.getLocalTransformation())
+
+        if print_mode == "mirror":
+            MirrorOperation(self, Vector(-1, 1, 1)).redo()
+            self.setPosition(Vector(-node_pos.x, node_pos.y, node_pos.z))
+        elif print_mode == "duplication":
+            self.setPosition(Vector(node_pos.x + (machine_width/2), node_pos.y, node_pos.z))
+
+        if node_pos.x > 0:
+            self.node.setPosition(Vector(0, node_pos.y, node_pos.z))
+        elif node_pos.x < -machine_width/2:
+            self.node.setPosition(Vector(-machine_width/2, node_pos.y, node_pos.z))
+
+
+    def _onTransformationChanged(self, node):
+        print_mode = Application.getInstance().getGlobalContainerStack().getProperty("print_mode", "value")
+        if print_mode != "regular":
+            self.update()
+
+    def _someParentChanged(self, node=None):
+        self.update()
---
+++ cura/PrintModeManager.py
@@ -0,0 +1,135 @@
+from UM.Application import Application
+from UM.Preferences import Preferences
+from UM.Signal import Signal
+from UM.Scene.SceneNode import SceneNode
+from UM.Operations.AddSceneNodeOperation import AddSceneNodeOperation
+from UM.Operations.RemoveSceneNodeOperation import RemoveSceneNodeOperation
+
+from cura.Settings.ExtruderManager import ExtruderManager
+
+class PrintModeManager:
+
+    def __init__(self):
+        super().__init__()
+        if PrintModeManager._instance is not None:
+            raise ValueError("Duplicate singleton creation")
+
+        PrintModeManager._instance = self
+
+        self._duplicated_nodes = []
+
+        old_material_id = Preferences.getInstance().getValue("cura/old_material")
+        if Application.getInstance().getContainerRegistry().findContainers(id=old_material_id):
+            self._old_material = Application.getInstance().getContainerRegistry().findContainers(id=old_material_id)[0]
+        else:
+            self._old_material = ""
+
+        self._global_stack = None
+        Application.getInstance().globalContainerStackChanged.connect(self._onGlobalStackChanged)
+        self._onGlobalStackChanged()
+
+        Application.getInstance().getMachineManager().activeMaterialChanged.connect(self._materialChanged)
+
+        self.printModeChanged.connect(self._onPrintModeChanged)
+        self._scene = Application.getInstance().getController().getScene()
+        # self._scene.sceneChanged.connect(self._onSceneChanged)
+        self._onPrintModeChanged()
+
+    def addDuplicatedNode(self, node):
+        self._duplicated_nodes.append(node)
+
+    def deleteDuplicatedNodes(self):
+        del self._duplicated_nodes[:]
+
+    def deleteDuplicatedNode(self, node):
+        self._duplicated_nodes.remove(node)
+
+    def getDuplicatedNode(self, node):
+        for node_dup in self._duplicated_nodes:
+            if node_dup.node == node:
+                return node_dup
+
+    def getDuplicatedNodes(self):
+        return self._duplicated_nodes
+
+    def renderDuplicatedNode(self, node):
+        if node.node.getParent() != self._scene.getRoot():
+            parent = self.getDuplicatedNode(node.node.getParent())
+        else:
+            parent = self._scene.getRoot()
+        op = AddSceneNodeOperation(node, parent)
+        op.redo()
+        node.update()
+
+    def renderDuplicatedNodes(self):
+        for node in self._duplicated_nodes:
+            self.renderDuplicatedNode(node)
+
+    def removeDuplicatedNodes(self):
+        for node in self._duplicated_nodes:
+            op = RemoveSceneNodeOperation(node)
+            op.redo()
+
+    def _onGlobalStackChanged(self):
+        if self._global_stack:
+            self._global_stack.propertyChanged.disconnect(self._onPropertyChanged)
+
+        self._global_stack = Application.getInstance().getGlobalContainerStack()
+
+        if self._global_stack:
+            self._global_stack.propertyChanged.connect(self._onPropertyChanged)
+            if not self._global_stack.getProperty("print_mode", "enabled"):
+                self.deleteDuplicatedNodes()
+
+    printModeChanged = Signal()
+
+    def _onPropertyChanged(self, key, property_name):
+        if key == "print_mode" and property_name == "value":
+            self.printModeChanged.emit()
+
+    def _onPrintModeChanged(self):
+        if self._global_stack:
+            print_mode = self._global_stack.getProperty("print_mode", "value")
+            nodes = self._scene.getRoot().getChildren()
+            if print_mode != "regular":
+                for node in nodes:
+                    if type(node) == SceneNode:
+                        self._setActiveExtruder(node)
+                if self._old_material == "":
+                    self._old_material = ExtruderManager.getInstance().getExtruderStack(1).material
+                    material = ExtruderManager.getInstance().getExtruderStack(0).material
+                    ExtruderManager.getInstance().getExtruderStack(1).setMaterial(material)
+                    Preferences.getInstance().setValue("cura/old_material", self._old_material.getId())
+                self.renderDuplicatedNodes()
+            else:
+                self.removeDuplicatedNodes()
+                if self._old_material != "":
+                    ExtruderManager.getInstance().getExtruderStack(1).setMaterial(self._old_material)
+                    self._old_material = ""
+                    Preferences.getInstance().setValue("cura/old_material", "")
+
+    def _materialChanged(self):
+        print_mode = self._global_stack.getProperty("print_mode", "value")
+        if print_mode != "regular":
+            material = ExtruderManager.getInstance().getExtruderStack(0).material
+            ExtruderManager.getInstance().getExtruderStack(1).setMaterial(material)
+
+    def _setActiveExtruder(self, node):
+        node.callDecoration("setActiveExtruder", ExtruderManager.getInstance().getExtruderStack(0).getId())
+
+    # def _onSceneChanged(self, node):
+    #     if self._global_stack:
+    #         print_mode = self._global_stack.getProperty("print_mode", "value")
+    #         if print_mode != "regular":
+    #             if type(node) == SceneNode:
+    #                 self._setActiveExtruder(node)
+
+    @classmethod
+    def getInstance(cls) -> "PrintModeManager":
+        # Note: Explicit use of class name to prevent issues with inheritance.
+        if not PrintModeManager._instance:
+            PrintModeManager._instance = cls()
+
+        return PrintModeManager._instance
+
+    _instance = None
